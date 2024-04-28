13f72f2915214a5c8a379eef7bb844c9a35ec556
Navneet Gupta <nnvkg107@gmail.com>
2024-04-28 20:34:22 +0530

    small header

--- app/project-brief/layout.js
+++ app/project-brief/layout.js
@@ -6,7 +6,7 @@ const layout = ({ children }) => {
   return (
     <div>
       <header
-        style={{ height: "10vh" }}
+        style={{ height: "5vh" }}
         className=" fixed top-0  z-[50] w-full border-b backdrop-blur-sm bg-white/[0.6] dark:bg-black/[0.6] border-neutral-200 dark:border-white/[0.1]"
       >
         <button className="m-0 border 1px h-full p-4 dark:border-white border-black rounded-sm">
