0278084e302f5336a55fc449cae6229335b6d10e
Navneet Gupta <nnvkg107@gmail.com>
2024-05-01 23:14:25 +0530

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-      <SpeedInsights/>
+        <SpeedInsights />
+        <Analytics/>
       </body>
     </html>
   );
